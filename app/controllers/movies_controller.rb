95423a1e9faca7dba380274109ac6f039a06d4ef
Raja Rao Tadimeti <t_rajarao@yahoo.com>
2012-03-18 23:48:36 -0700

    Added session support to remember user selections Implemented the enhancements of hw2 part5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,25 +10,43 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     logger.debug("all ratings: #{@all_ratings}")
 
-    @hilite_col = nil
+    session_params_used = false
 
+    @hilite_col = nil
     if (@selected_ratings_hash == nil)
-      logger.debug("RAJA: allocating selected_ratings hash")
+      logger.debug("allocating selected_ratings hash")
       @selected_ratings_hash = Hash.new
     end
 
-    find_args = Hash.new
-
     if params.has_key?(:ratings)
-      @selected_ratings_hash = params[:ratings]
-      logger.debug("selected ratings: #{@selected_ratings_hash.keys}")
-      find_args[:conditions]=Hash[:rating => @selected_ratings_hash.keys]
+      @selected_ratings_hash = session[:selected_ratings_hash] = 
+        params[:ratings]
+      logger.debug("selected p ratings: #{@selected_ratings_hash.keys}")
+    elsif (session[:selected_ratings_hash] != nil)
+      session_params_used = true
+      @selected_ratings_hash = session[:selected_ratings_hash]
+      logger.debug("selected s ratings: #{@selected_ratings_hash.keys}")
     end
 
     if params.has_key?(:sort)
-      @hilite_col = params[:sort]
-      find_args[:order]=params[:sort]
+      @hilite_col = session[:sort] = params[:sort]
+      logger.debug("selected p sort: #{@hilite_col}")
+    elsif (session[:sort] != nil)
+      session_params_used = true
+      @hilite_col = session[:sort]
+      logger.debug("selected s sort: #{@hilite_col}")
     end
+
+    if (session_params_used == true) 
+    # redirect to the new RESTful url as params have changed
+      logger.debug("at least one p is restored from s, redirecting..")
+      redirect_to movies_path({:sort=>@hilite_col,
+                               :ratings=>@selected_ratings_hash})
+    end
+
+    find_args = Hash.new
+    find_args[:conditions]=Hash[:rating => @selected_ratings_hash.keys]
+    find_args[:order]=@hilite_col
 
     logger.debug("find args: #{find_args}")
     @movies = Movie.find(:all, find_args)
